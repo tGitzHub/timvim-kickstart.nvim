1480289b64ce014bed4fbaf40c729aa90ad0ecea
tGitzHub <56638472+tGitzHub@users.noreply.github.com>
2023-07-20 22:00:45 +0200

    add <leader>m for :Ex and turn on relativenumbers

--- init.lua
+++ init.lua
@@ -215,6 +215,7 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
@@ -301,6 +302,7 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>m', vim.cmd.Ex, { desc = 'Open File [M]enu'})
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
